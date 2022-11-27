4af37f4ba78c5a92a5e823dfecfb83c3990da682
Léo Bruant <bruantleo@gmail.com>
2022-11-27 01:38:23 +0100

    Added lightswitcher

--- svelte-kit/src/routes/+layout.svelte
+++ svelte-kit/src/routes/+layout.svelte
@@ -2,40 +2,22 @@
     import '../theme.postcss';
     import '@brainandbones/skeleton/styles/all.css';
 
-    import { AppShell, AppBar, GradientHeading, RadioGroup, RadioItem } from '@brainandbones/skeleton';
-    import { writable } from 'svelte/store';
-    import { onMount } from 'svelte';
-
-    const theme = writable('dark');
-
-    onMount(() => {
-        const html = document.querySelector('html');
-
-        theme.subscribe((value) => {
-            if(!html?.classList.contains(value)){
-                html?.classList.toggle('dark');
-                html?.classList.toggle('light');
-            }
-        })
-    });
+    import { AppShell, AppBar, GradientHeading, LightSwitch } from '@brainandbones/skeleton';
 </script>
 
 <AppShell>
     <AppBar slot="header" class="nav">
         <svelte:fragment slot="lead">
             <div class="items-center flex gap-3">
-                <img class="h-9" src="logo.png" alt="logo">
-                <GradientHeading tag="h2" direction="bg-gradient-to-r" from="from-primary-500" to="to-tertiary-500">
+                <img class="h-8" src="logo.png" alt="logo">
+                <GradientHeading class="text-3xl" tag="span" direction="bg-gradient-to-r" from="from-primary-500" to="to-tertiary-500">
                     RankyList
                 </GradientHeading>
             </div>
         </svelte:fragment>
         <svelte:fragment slot="trail">
             <div class="flex gap-5 items-center">
-                <RadioGroup selected={theme}>
-                    <RadioItem value="dark">Dark</RadioItem>
-                    <RadioItem value="light">Light</RadioItem>
-                </RadioGroup>
+                <LightSwitch />
                 <a href="login">Login</a>
             </div>
         </svelte:fragment>    
